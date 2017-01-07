78ab1139eda16d5d0ee47c0d0f3903e7b0172cd7
Adebayo Ganiyat <abukky52@yahoo.com>
2017-01-07 00:36:13 +0000

    Add files via upload

---
+++ SSP (1).py
@@ -0,0 +1,64 @@
+from random import randint, sample
+from itertools import chain, combinations
+from time import time
+
+class SSP():
+    def __init__(self, S=[], t=0):
+        self.S = S
+        self.t = t
+        self.n = len(S)
+        #
+        self.decision = False
+        self.total    = 0
+        self.selected = []
+
+    def __repr__(self):
+        return "SSP instance: S="+str(self.S)+"\tt="+str(self.t)
+    
+    def random_instance(self, n, bitlength=10):
+        max_n_bit_number = 2**bitlength-1
+        self.S = sorted( [ randint(0,max_n_bit_number) for i in range(n) ] , reverse=True)
+        self.t = randint(0,n*max_n_bit_number)
+        self.n = len( self.S )
+
+    def random_yes_instance(self, n, bitlength=10):
+        max_n_bit_number = 2**bitlength-1
+        self.S = sorted( [ randint(0,max_n_bit_number) for i in range(n) ] , reverse=True)
+        self.t = sum( sample(self.S, randint(0,n)) )
+        self.n = len( self.S )
+
+    ###
+
+    def try_at_random(self):
+        candidate = []
+        total = 0
+        while total != self.t:
+            candidate = sample(self.S, randint(0,self.n))
+            total     = sum(candidate)
+            print( "Trying: ", candidate, ", sum:", total )
+
+    def greedy(self, n, bitlength=10):
+        max_n_bit_number = 2**bitlength-1
+        self.S = sorted( [ randint(0,max_n_bit_number) for i in range(n) ] , reverse=True)
+        total = 0
+        self.n = len( self.S)
+        self.occurance = [0,n-1,-1]
+        while i in self.S and i  in self.occurance:
+            t = total + self.S[i]
+            if t >= total + self.S[i]:
+                total = total + self.S[i]
+        return None
+        print ("sol: ", t-total, "Trying: ", self.candidate, ", sum:", self.total)
+            
+
+instance = SSP()
+instance.greedy(4)
+instance.random_yes_instance(4)
+print( instance )
+
+instance.try_at_random()
+
+for n in range(10,40):
+    instance.random_yes_instance(n)
+    instance.greedy(4)
+    print instance.greedy(4)
