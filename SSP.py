2397d1a1b0a3cd1404a69a438e80166f04c21ae2
rebeccaogun <rebeccaogun@outlook.com>
2017-01-09 19:37:50 +0000

    Update dynamic
    working on random function

--- SSP.py
+++ SSP.py
@@ -1,3 +1,4 @@
+import random
 from random import randint, sample
 from itertools import chain, combinations
 from time import time
@@ -11,23 +12,6 @@ class SSP():
         self.decision = False
         self.total    = 0
         self.selected = []
-
-    def subset_sum(sequence, sum_value):
-        cols = sum_value + 1         # Plus 1 for 0 valued col.
-        rows = len(sequence) + 1     # Plus 1 for 0 valued row.
-        T = [[False for _ in range(cols)] for _ in range(rows)]
-
-        for row in range(rows):
-            T[row][0] = True
-
-        for index_i in range(1, rows):
-            for index_j in range(1, cols):
-                if index_j >= sequence[index_i - 1]:
-                    T[index_i][index_j] = T[index_i - 1][index_j] or T[index_i - 1][index_j - sequence[index_i - 1]]
-                else:
-                    T[index_i][index_j] = T[index_i - 1][index_j]
-
-        return T[rows - 1][cols - 1]
 
     def __repr__(self):
         return "SSP instance: S="+str(self.S)+"\tt="+str(self.t)
@@ -46,17 +30,20 @@ class SSP():
 
     ###
 
-    def try_at_random(self):
-        candidate = []
-        total = 0
-        while total != self.t:
-            candidate = sample(self.S, randint(0,self.n))
-            total     = sum(candidate)
-            print( "Trying: ", candidate, ", sum:", total )
+    def try_at_random(self, S, n, t):
+        if (t == 0):
+            return True
+            print("Subset has correct sum")
+        if (n == 0 and t!=0):
+            return False
+            print("No subset has correct sum")
+
+        
+        
+  
+
             
 
-if __name__ == '__main__':
-    sequence = [2, 3, 7, 8]
-    assert True == subset_sum(sequence, 11)
-    
-
+instance = SSP()
+instance.random_yes_instance(4)
+print( instance )
